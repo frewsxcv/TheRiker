94539aa09f7a19f5e108e2a14d6a41df0e5d597d
Peter Faiman <peter@mint>
2012-03-01 22:41:35 -0800

    regex now works, needs cleanup though

--- theriker.py
+++ theriker.py
@@ -10,7 +10,7 @@ import sys, re
 
 class FunctionBot(IRCClient):
     function_regex = re.compile('!(\w+)\s*')
-    regex_regex = re.compile('s/([^/]*)/([^/]*)(?:/(g|i)?)?\s+(\w+)')
+    regex_regex = re.compile('s/([^/]*)/([^/]*)(?:/([ig]*)(?:\s+(\w+))?)?')
 
     def signedOn(self):
         for c in self.factory.settings.CHANNELS:
@@ -21,17 +21,34 @@ class FunctionBot(IRCClient):
             reactor.callLater(self.factory.settings.REJOIN_DELAY, self.join, channel)
 
     def privmsg(self, user, channel, msg):
+        user = user[:user.index('!')]
         mo = self.function_regex.match(msg)
         if mo:
             name = mo.group(1)
             arg = msg[mo.end(0):]
-            user = user[:user.index('!')]
             self.msg(channel, 'Function %s called with arg "%s" by %s.' % (mo.group(1), arg, user))
             return
         mo = self.regex_regex.match(msg)
         if mo:
-            self.msg(channel, 'Regex! "%s" "%s" "%s" for %s' % mo.group(1, 2, 3, 4))
+            pat, repl, opt, target = mo.group(1, 2, 3, 4)
+            count = 1
+            flags = 0
+            if target == None:
+                target = user
+            if (channel+user) not in self.linelog:
+                return
+            if opt != None:
+                if opt.count('g') == 1:
+                    count = 0
+                if opt.count('i') == 1:
+                    flags = re.I
+            msg, subs = re.subn(pat, repl, self.linelog[channel+target], count, flags)
+            if subs > 0:
+                self.msg(channel, '<%s> %s' % (target, msg))
+                self.linelog[channel+target] = msg
+            self.msg(channel, 'Regex! "%s" "%s" "%s" for %s' % (pat,repl,opt,target))
             return
+        self.linelog[channel+user] = msg
             
 
 class FunctionBotFactory(ReconnectingClientFactory):
@@ -46,6 +63,7 @@ class FunctionBotFactory(ReconnectingClientFactory):
         p.password = self.settings.PASSWORD
         p.realname = self.settings.REALNAME
         p.username = self.settings.USERNAME
+        p.linelog = {}
         return p
 
 if __name__ == '__main__':
